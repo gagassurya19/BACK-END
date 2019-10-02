8db7802d521640e6b111227f81b5952fa536ac5d
Gagas Surya Laksana <Gagassurya19@yahoo.com>
2019-10-02 23:48:16 +0700

    unsolved(striped)

--- BelajarOOP/src/propertyOfString/main.java
+++ BelajarOOP/src/propertyOfString/main.java
@@ -5,16 +5,16 @@ public class main {
         String balik = "y";
         while(balik.equals("y")){
             System.out.println("Sedang memuat...");
-        Scanner input = new Scanner(System.in);
-        System.out.print("masukkan kalimat : ");
-        String inputan = input.next();
-        proses olah = new proses(inputan);
-        olah.balik();
-        olah.vokal();
-        olah.konsonan();
-        olah.upper();
-        olah.lower();
-        olah.striped();
+            Scanner input = new Scanner(System.in);
+            System.out.print("masukkan kalimat : ");
+            String inputan = input.next();
+            proses olah = new proses(inputan); //membuat objek baru dan menghubungkan class
+            olah.balik();
+            olah.vokal();
+            olah.konsonan();
+            olah.upper();
+            olah.lower();
+            olah.striped();
             System.out.println("ingin mengulang ? (y/t)");
             balik = input.next();
         } 
